de9c58a845f142280b3376e0067ca49cb423a628
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-18 20:41:57 -0300

    core: the companies, users, subscription, payments directories were created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getDatabaseConfig } from './config/database.config';
 
+
+/**
+ * Modulo raiz de la aplicación
+ * 
+ */
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }), // carga variables de entorno automaticamtne desde .env
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
