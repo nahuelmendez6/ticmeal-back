cddf7e5705621275491f58327ba3c56a2dd86aa2
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-23 19:35:47 -0300

    feat(user): create CreateUserDto

---
+++ src/modules/users/dto/create.user.dto.ts
@@ -0,0 +1,30 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsEnum(['super_admin', 'company_admin', 'diner', 'kitchen', 'kitchen_admin'])
+  role?: string;
+
+  @IsOptional()
+  @Matches(/^\d{4}$/, { message: 'El PIN debe tener exactamente 4 dígitos numéricos' })
+  pin?: string;
+}
