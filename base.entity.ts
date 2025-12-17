c2d61f04d3ea9e8a0051997a3b3c273e2d049df8
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-12-17 20:20:14 -0300

    fix: fixed tsconfig

--- base.entity.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export abstract class BaseEntity {
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-  createdAt: Date;
-
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-  updatedAt: Date;
-}
